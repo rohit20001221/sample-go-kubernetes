b82971ac44aa5cfc353e9e032d5a0430bda001ec
rohit20001221 <rohit20001221@gmail.com>
2023-11-15 02:14:43 +0530

    builderx env command

--- build/build-docker-image.sh
+++ build/build-docker-image.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+docker buildx use go-kubernetes
 
 docker buildx build \
     --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v8 \
---
+++ build/builderx-env.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker buildx create --name go-kubernetes
